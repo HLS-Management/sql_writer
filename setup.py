d98dad6546ffca015f5077c94a8744674c6e8578
Hamish Songsmith <hamish_songsmith@icloud.com>
2022-04-14 22:41:17 +1000

    Prepared all of the testing

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="sql_writer",
-    version="0.0.1",
-    author="HLS Management",
-    description="package that spits out SQL so you dont have to write it",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/HLS-Management/sql_writer",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.9',
-)
+if __name__ == "__main__":
+    setup()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,16 @@
 import pytest
-from ..source import basic
+from sql_writer import sql
 
 
 class Test_Select:
     def test_select_all(self):
-        assert basic.select("table") == "SELECT * FROM table"
+        assert sql.select("table") == "SELECT * FROM table"
 
     def test_select_specific(self):
-        assert basic.select("table", ["col1, col2"]) == "SELECT col1, col2 FROM table"
+        assert sql.select("table", ["col1, col2"]) == "SELECT col1, col2 FROM table"
 
     def test_select_where(self):
-        assert basic.select("table", query_fields=["col1", "col2"]) == "SELECT * FROM table WHERE col1 = %s AND col2 = %s"
+        assert sql.select("tbl", query_fields=["col1", "col2"]) == "SELECT * FROM tbl WHERE col1 = %s AND col2 = %s"
 
     @pytest.mark.parametrize("invalid_tables", [
         "",
@@ -20,7 +20,7 @@ class Test_Select:
         ])
     def test_select_table_type(self, invalid_tables):
         with pytest.raises(Exception):
-            basic.select(invalid_tables)
+            sql.select(invalid_tables)
 
     @pytest.mark.parametrize("invalid_fields", [
         "",
@@ -31,7 +31,7 @@ class Test_Select:
         ])
     def test_select_field_type(self, invalid_fields):
         with pytest.raises(Exception):
-            basic.select("test", invalid_fields)
+            sql.select("test", invalid_fields)
 
     @pytest.mark.parametrize("invalid_fields", [
         "",
@@ -42,4 +42,70 @@ class Test_Select:
         ])
     def test_select_where_field_type(self, invalid_fields):
         with pytest.raises(Exception):
-            basic.select("test", query_fields=invalid_fields)
+            sql.select("test", query_fields=invalid_fields)
+
+
+class Test_Insert:
+    def test_insert(self):
+        assert sql.insert("table", ["col1", "col2"]) == "INSERT INTO table (col1, col2) VALUES (%s, %s)"
+
+    @pytest.mark.parametrize("invalid_tables", [
+        "",
+        ["Bob"],
+        {"Never odd or even"},
+        ("Do geese see God?",)
+        ])
+    def test_insert_table_type(self, invalid_tables):
+        with pytest.raises(Exception):
+            sql.insert(invalid_tables, [1, 2])
+
+    @pytest.mark.parametrize("invalid_fields", [
+        "",
+        "test",
+        {"Never odd or even"},
+        ("Do geese see God?",),
+        [1, ["wow"]]
+        ])
+    def test_insert_field_type(self, invalid_fields):
+        with pytest.raises(Exception):
+            sql.insert("test", invalid_fields)
+
+
+class Test_Update:
+    def test_update_where(self):
+        assert sql.update("tbl", [1, 2], [3, 4]) == "UPDATE tbl SET 1 = %s, 2 = %s WHERE 3 = %s AND 4 = %s"
+
+    def test_update(self):
+        assert sql.update("tbl", [1, 2]) == "UPDATE tbl SET 1 = %s, 2 = %s"
+
+    @pytest.mark.parametrize("invalid_tables", [
+        "",
+        ["Bob"],
+        {"Never odd or even"},
+        ("Do geese see God?",)
+        ])
+    def test_update_table_type(self, invalid_tables):
+        with pytest.raises(Exception):
+            sql.update(invalid_tables, [1])
+
+    @pytest.mark.parametrize("invalid_fields", [
+        "",
+        "test",
+        {"Never odd or even"},
+        ("Do geese see God?",),
+        [1, ["wow"]]
+        ])
+    def test_update_field_type(self, invalid_fields):
+        with pytest.raises(Exception):
+            sql.update("test", invalid_fields)
+
+    @pytest.mark.parametrize("invalid_fields", [
+        "",
+        "test",
+        {"Never odd or even"},
+        ("Do geese see God?",),
+        [1, ["wow"]]
+        ])
+    def test_update_where_field_type(self, invalid_fields):
+        with pytest.raises(Exception):
+            sql.update("test", [1, 2], query_fields=invalid_fields)
